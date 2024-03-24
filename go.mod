0e398a03160984ae27e2d11756e95628c2d221d7
Andrew Mushel <almushel@protonmail.com>
2024-03-24 13:06:44 -0500

    Node types and parsing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/almushel/inertHTML
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	txt := TextNode{
+		Text:     "Lorem Ipsum",
+		TextType: "strong",
+		URL:      "https://www.inerthtml.com",
+	}
+
+	html := HtmlNode{
+		Tag:   "p",
+		Value: "Lorem Ipsum",
+		Props: map[string]string{
+			"href":   "https://www.inerthtml.com",
+			"target": "_blank",
+		},
+	}
+
+	fmt.Printf("%#v\n", txt)
+	fmt.Printf("%#v\n", html)
+	fmt.Println(html.ToHTML())
+}
